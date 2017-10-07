6dabb3535267de111debf35ac0ff32ba8f75c74f
Frederikam <frogkr@gmail.com>
2017-10-07 15:19:55 +0200

    Rewrote ShardIdentifier

--- src/main/java/fredboat/dike/cache/ShardIdentifier.java
+++ src/main/java/fredboat/dike/cache/ShardIdentifier.java
@@ -5,16 +5,48 @@
 
 package fredboat.dike.cache;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fredboat.dike.util.Const;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class ShardIdentifier {
 
+    private final String token;
     private final long user;
     private final int shardId;
     private final int shardCount;
 
-    public ShardIdentifier(long user, int shardId, int shardCount) {
+    public ShardIdentifier(String token, long user, int shardId, int shardCount) {
+        this.token = token;
         this.user = user;
         this.shardId = shardId;
         this.shardCount = shardCount;
+    }
+
+    public static ShardIdentifier getFromToken(String token, int shardId, int shardCount) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        ObjectMapper mapper = new ObjectMapper();
+
+        Request request = new Request.Builder()
+                .url(Const.API_GET_CURRENT_USER)
+                .header("Authorization", "Bot " + token)
+                .header("User-Agent", Const.USER_AGENT)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        //noinspection ConstantConditions
+        long user = new JSONObject(response.body()).getLong("id");
+
+        return new ShardIdentifier(token, user, shardId, shardCount);
+    }
+
+    public String getToken() {
+        return token;
     }
 
     public long getUser() {
